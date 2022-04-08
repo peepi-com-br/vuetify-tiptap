e7d42252e22742b536453d0fede947d7f27863c4
Ricardo Faust <ricardocfaust@gmail.com>
2022-04-08 19:25:36 -0300

    Chromatic changes

--- src/VTiptap.vue
+++ src/VTiptap.vue
@@ -19,6 +19,7 @@
           borderColor: $attrs['error-messages'] ? '#ff5252' : undefined,
         }"
       >
+        <v-btn>Testing changes in chromatic</v-btn>
         <!-- Toolbar -->
         <v-toolbar
           v-if="!hideToolbar && toolbar.length"
