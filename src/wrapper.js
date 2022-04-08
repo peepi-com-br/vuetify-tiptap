cf88c9cb67ee1bae2167379adecab1df976d567d
a21ns1g4ts <atila.danvi@outlook.com>
2022-04-08 15:59:17 -0300

    Add rollup config

---
+++ src/wrapper.js
@@ -0,0 +1,23 @@
+import VTiptap from "./VTiptap.vue";
+
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("VTiptap", VTiptap);
+}
+
+const plugin = {
+  install,
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+export default VTiptap;
