ebbd4821a625e6ebf2aa584e4a0034a7e876b707
Ricardo Faust <ricardocfaust@gmail.com>
2022-04-20 10:23:00 -0300

    Fix xssOptions

--- src/components/VTiptap.vue
+++ src/components/VTiptap.vue
@@ -284,7 +284,7 @@ export default class extends Vue {
       .replace("https://vimeo.com/", "https://player.vimeo.com/video/");
 
     let whiteList = this.xssOptions;
-    if (Array.isArray(this.xssOptions)) {
+    if (Array.isArray(this.xssOptions) && Array.isArray(this.xss)) {
       whiteList = this.xss.reduce((acc, rule) => {
         acc[rule] = this.xssOptions[rule];
         return acc;
