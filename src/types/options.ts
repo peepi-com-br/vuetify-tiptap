cafc8d52213388e9d044ffdbfe6d21af65638760
a21ns1g4ts <atila.danvi@outlook.com>
2022-09-22 16:51:06 -0300

    Fix type

--- src/types/options.ts
+++ src/types/options.ts
@@ -1,4 +1,4 @@
 export interface PluginOptions {
   uploadImage: (file: File) => Promise<string> | null;
-  mentionItems: (items: object[]) => Promise<object[]> | null;
+  mentionItems: (query: string) => Promise<object[]> | null;
 }
